7a1a52e2bcc1388ecec484fec5024ce97890a6d3
hilman-firdd <hilmanfirdaus48@gmail.com>
2023-01-01 22:25:27 +0700

    #1 | adding template

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\BerandaWaliController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaWaliController;
+use App\Http\Controllers\BerandaOperatorController;
 
 /*
 |--------------------------------------------------------------------------
